f393d70f343b85cc4571524f280a7e828d1e2b0b
Ayub-Tech <Ayubmohamud0920@gmail.com>
2025-02-22 17:58:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Groupwork____Ayub_Mohamud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
